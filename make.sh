f12f779e88c2dcc397e47b8e56d6da79d2fe3165
McBane87 <McBane87@users.noreply.github.com>
2015-06-10 15:38:12 +0200

    Fixed --cmdline in make.sh

--- make.sh
+++ make.sh
@@ -28,7 +28,7 @@ ARCH=arm CROSS_COMPILE=./arm-eabi-4.6/bin/arm-eabi- make
 
 ## Tablet Z
 if [[ $Target == 311 ]]; then
-./mkbootimg --kernel arch/arm/boot/zImage --ramdisk kernel.sin-ramdisk-SGP311.cpio.gz --pagesize 2048 --base 0x80200000 --cmdline "androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 vmalloc=400M androidboot.emmc=true" --ramdisk_offset 0x02000000 --output boot.img
+./mkbootimg --kernel arch/arm/boot/zImage --ramdisk kernel.sin-ramdisk-SGP311.cpio.gz --pagesize 2048 --base 0x80200000 --cmdline "androidboot.hardware=qcom user_debug=23 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.selinux=permissive vmalloc=400M  androidboot.bootdevice=msm_sdcc.1" --ramdisk_offset 0x02000000 --output boot.img
 else
-./mkbootimg --kernel arch/arm/boot/zImage --ramdisk kernel.sin-ramdisk-SGP321.cpio.gz --pagesize 2048 --base 0x80200000 --cmdline "androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 vmalloc=400M androidboot.emmc=true" --ramdisk_offset 0x02000000 --output boot.img
+./mkbootimg --kernel arch/arm/boot/zImage --ramdisk kernel.sin-ramdisk-SGP321.cpio.gz --pagesize 2048 --base 0x80200000 --cmdline "androidboot.hardware=qcom user_debug=23 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.selinux=permissive vmalloc=400M  androidboot.bootdevice=msm_sdcc.1" --ramdisk_offset 0x02000000 --output boot.img
 fi
